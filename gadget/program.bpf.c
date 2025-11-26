cbf720ce43dee91db4c66fb556ed5b978608222d
Alban Crequy <alban.crequy@gmail.com>
2025-11-26 18:28:20 +0100

    General updates to new IG
    Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

--- gadget/program.bpf.c
+++ gadget/program.bpf.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
-/* Copyright (c) 2024 The Inspektor Gadget authors */
+/* Copyright (c) 2024-2025 The Inspektor Gadget authors */
 
 #include <vmlinux.h>
 
@@ -7,8 +7,10 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+#include <gadget/common.h>
 #include <gadget/buffer.h>
 #include <gadget/macros.h>
+#include <gadget/mntns.h>
 #include <gadget/mntns_filter.h>
 #include <gadget/types.h>
 
@@ -16,42 +18,54 @@
 #define TASK_COMM_LEN 16
 #endif
 
+enum cve_t {
+  // https://github.com/opencontainers/runc/security/advisories/GHSA-xr7r-f8xq-vfvv
+  CVE_2024_21626 = 0,
+};
+
 struct event {
   gadget_timestamp timestamp;
-  gadget_mntns_id mntns_id;
-  __u32 pid;
-  __u8 comm[TASK_COMM_LEN];
-  __u8 path[100];
+  struct gadget_process proc;
+  enum cve_t cve_raw;
+  char details[128];
 };
 
 #ifndef SIGKILL
 #define SIGKILL 9
 #endif
 
-const volatile int target_signal = SIGKILL;
-
-GADGET_PARAM(target_signal);
+const volatile bool kill = false;
+GADGET_PARAM(kill);
 
 GADGET_TRACER_MAP(events, 1024 * 256);
 
-GADGET_TRACER(signalgogadget, events, event);
+GADGET_TRACER(runcwatcher, events, event);
 
-SEC("tracepoint/syscalls/sys_enter_chdir")
-int tracepoint__sys_enter_chdir(struct trace_event_raw_sys_enter *ctx) {
-  struct event *event;
-  __u64 pid_tgid = bpf_get_current_pid_tgid();
-  __u8 comm[TASK_COMM_LEN];
-  __u8 pattern[15] = "/proc/self/fd/";
-  __u8 path[15];
-  int ret;
+static __always_inline int is_runc() {
+  char comm[TASK_COMM_LEN];
 
   bpf_get_current_comm(&comm, sizeof(comm));
+
   /* runc:[2:INIT] */
   if (comm[0] != 'r' || comm[1] != 'u' || comm[2] != 'n' || comm[3] != 'c' ||
       comm[4] != ':' || comm[5] != '[' || comm[6] != '2' || comm[7] != ':' ||
       comm[8] != 'I' || comm[9] != 'N' || comm[10] != 'I' || comm[11] != 'T' ||
       comm[12] != ']' || comm[13] != '\0')
     return 0;
+
+  return 1;
+}
+
+SEC("tracepoint/syscalls/sys_enter_chdir")
+int tracepoint__sys_enter_chdir(struct trace_event_raw_sys_enter *ctx) {
+  struct event *event;
+
+  if (!is_runc())
+    return 0;
+
+  char pattern[15] = "/proc/self/fd/";
+  char path[15];
+  int ret;
 
   ret = bpf_probe_read_user_str(path, sizeof(path), (void *)ctx->args[0]);
   if (ret <= 0)
@@ -70,18 +84,18 @@ int tracepoint__sys_enter_chdir(struct trace_event_raw_sys_enter *ctx) {
     return 0;
 
   /* event data */
+  gadget_process_populate(&event->proc);
+  event->cve_raw = CVE_2024_21626;
   event->timestamp = bpf_ktime_get_boot_ns();
-  event->mntns_id = gadget_get_mntns_id();
-  event->pid = pid_tgid >> 32;
-  bpf_get_current_comm(&event->comm, sizeof(event->comm));
-  bpf_probe_read_user_str(&event->path, sizeof(event->path),
+  bpf_probe_read_user_str(&event->details, sizeof(event->details),
                           (void *)ctx->args[0]);
 
   /* emit event */
   gadget_submit_buf(ctx, &events, event, sizeof(*event));
 
-  /* signal target process */
-  bpf_send_signal(target_signal);
+  if (kill)
+    bpf_send_signal(SIGKILL);
+
   return 0;
 }
 
